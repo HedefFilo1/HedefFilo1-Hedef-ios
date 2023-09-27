92bfcae7f70c9419a9bd57d70d9145217bca14e0
jamal zare <jamal.zare@solidict.com>
2023-09-27 20:24:06 +0330

    create project

---
+++ Copilot/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Copilot
+//
+//  Created by Jamal on 9/27/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
