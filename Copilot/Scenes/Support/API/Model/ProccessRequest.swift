84818462d9dda78ede9a1d6111476e94b31bddb9
jamalzare514@gmail.com <jamal.zare@solidict.com>
2024-05-26 14:06:03 +0330

    HED-1319

--- Copilot/Scenes/Support/API/Model/ProccessRequest.swift
+++ Copilot/Scenes/Support/API/Model/ProccessRequest.swift
@@ -7,6 +7,66 @@
 
 import Foundation
 
+enum WebCategoryEnum: String, Decodable {
+    case failur = "FAILURE"
+    case damage = "DAMAGE"
+    case tire = "TIRE"
+    case maintenance = "MAINTENANCE"
+    case invoiceObjection = "INVOICE_OBJECTION"
+    case logo = "LOGO"
+    case inspection = "INSPECTION"
+    case ogsHgs = "OGS_HGS"
+    case missingPlate = "MISSING_PLATE"
+    case missingLicense = "MISSING_LICENSE"
+    case foreignExit = "FOREIGN_EXIT"
+    case penaltyInvoice = "PENALTY_INVOICE"
+    case vehicleOccupation = "VEHICLE_OCCUPATION"
+    case vehicleInspection = "VEHICLE_INSPECTION"
+    case trafficPolicy = "TRAFFIC_POLICY"
+    case vehicleReturn = "VEHICLE_RETURN"
+    case occupiedVehicle = "OCCUPIED_VEHICLE"
+    
+    var title: String {
+        switch self {
+        case .failur:
+            return "Arıza"
+        case .damage:
+            return "Hasar"
+        case .tire:
+            return "Tamir"
+        case .maintenance:
+            return "Bakım"
+        case .invoiceObjection:
+            return "INVOICE_OBJECTION"
+        case .logo:
+            return "Logo"
+        case .ogsHgs:
+            return "OGS_HGS"
+        case .missingPlate:
+            return "MISSING_PLATE"
+        case .missingLicense:
+            return "MISSING_LICENSE"
+        case .foreignExit:
+            return "FOREIGN_EXIT"
+        case .penaltyInvoice:
+            return "PENALTY_INVOICE"
+        case .vehicleOccupation:
+            return "VEHICLE_OCCUPATION"
+        case .vehicleInspection:
+            return "VEHICLE_INSPECTION"
+        case .trafficPolicy:
+            return "TRAFFIC_POLICY"
+        case .vehicleReturn:
+            return "VEHICLE_RETURN"
+            
+        case .occupiedVehicle:
+            return App.getString(key: "copilotapp.demandlist.product.process.request.towed.vehicle.operation") ?? "Bağlanan Araç İşlemleri"
+        case .inspection:
+            return "INSPECTION"
+        }
+    }
+}
+
 struct ProccessRequest: Decodable {
     let id: String?
     let supplierName: String?
@@ -16,8 +76,13 @@ struct ProccessRequest: Decodable {
     let createdDate: String?
     let recordType: String?
     let invoiceNumber: String?
+    let webCategoryEnum: WebCategoryEnum?
     
     var displayTitle: String {
+        if let webCategoryEnum {
+            return webCategoryEnum.title
+        }
+        
         if let supplierName {
             return supplierName
         }
